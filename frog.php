e2090737b3032aa97beaa3aecf7d8fc4a034f467
Henryrivardo07 <henrysihombing20@gmail.com>
2024-10-04 10:00:05 +0700

    tugas-9

---
+++ frog.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+        class Frog extends Animal{
+            public $legs = 4;
+            public function jump(){
+                echo "Hop Hop";
+            }
+        }
+
+        
+    ?>
+</body>
+</html>
